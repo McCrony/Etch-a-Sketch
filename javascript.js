e6590e9483ff19672e7934cba2968f05cfd703e5
McCrony <askmccrony@gmail.com>
2024-06-25 17:12:43 +0000

    Add top menu buttons

--- javascript.js
+++ javascript.js
@@ -1,19 +1,20 @@
 const grid = document.querySelector(".grid");
 
-function createSquares(size) {
+createDefaultGrid(2)
+
+// Function to create default 16x16 grid
+function createDefaultGrid(size) {
   const container = document.querySelector('.grid');
 
-  // const squareSize = `${windowsize / size, windowsize / size}px`; // Calculate square size based on viewport dimensions
-  const squareSize = `${500 / size}px`; // Calculate square size based on viewport dimensions
+  const squareSize = `${500 / size}px`; 
 
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement('div');
     square.style.width = squareSize;
     square.style.height = squareSize;
-    square.classList.add('gridCell'); // Add a class for styling
+    square.classList.add('gridCell');
     container.appendChild(square);
   }
 }
 
-createSquares(16);
 
