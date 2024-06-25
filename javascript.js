3ef8e991e57b340c8047583673a3893535eb264c
McCrony <askmccrony@gmail.com>
2024-06-25 17:17:01 +0000

    Edit createDefaulGrid function to accept variable

--- javascript.js
+++ javascript.js
@@ -1,14 +1,15 @@
 const grid = document.querySelector(".grid");
+const gridSize = 12;
 
-createDefaultGrid(2)
+createDefaultGrid(gridSize);
 
 // Function to create default 16x16 grid
-function createDefaultGrid(size) {
+function createDefaultGrid(gridSize) {
   const container = document.querySelector('.grid');
 
-  const squareSize = `${500 / size}px`; 
+  const squareSize = `${500 / gridSize}px`; 
 
-  for (let i = 0; i < size * size; i++) {
+  for (let i = 0; i < gridSize * gridSize; i++) {
     const square = document.createElement('div');
     square.style.width = squareSize;
     square.style.height = squareSize;
