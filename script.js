60d8c30cddca87e7793bb942c881068d5ea6c25f
McCrony <askmccrony@gmail.com>
2024-07-02 04:12:17 +0000

    Add Random RGB colour function and fav icon

--- script.js
+++ script.js
@@ -10,12 +10,10 @@ const pickColour = document.querySelector('input[type=color');
 const randomColour = document.querySelector('input[type=checkbox');
 
 //Random RGB valuse
-let red = Math.floor(Math.random()*256);
-let green = Math.floor(Math.random()*256);
-let blue = Math.floor(Math.random()*256);
-let rgb = `rgb(${red}, ${green}, ${blue})`
-
-
+function generateRandomRGB(){
+  let randomColor = Math.floor(Math.random() * 256);
+  return randomColor;
+}
 // Generate new grid
 createBtn.addEventListener('click', () => {
   if (userNumber.value < 1 || userNumber.value > 100) {
@@ -42,7 +40,7 @@ function fillCell () {
   gridCells.forEach((gridCell) => {
     gridCell.addEventListener('mouseover', ()=>{
       if(randomColour.checked == true){
-        gridCell.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+      gridCell.style.backgroundColor = `rgb(${generateRandomRGB()}, ${generateRandomRGB()}, ${generateRandomRGB()})`;
       }else{
         gridCell.style.backgroundColor = pickColour.value;
       }
